267c71d59a1389774e820cdc88f744e8959ef874
cyjjj <cyd5538@naver.com>
2022-04-19 22:04:50 +0900

    .

--- corona/src/components/Home/Koreachart.js
+++ corona/src/components/Home/Koreachart.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React from "react";
 
 function KoreaChart() {
-    return (
-        <div className='flex justify-center items-center'>
-            <canvas className='shadow-2xl w-4/5 mt-10 border-2 w-full'>
-                asdaasdasd
-            </canvas>
+  const dataList = [
+    {
+      name: "확진자",
+    },
+    {
+      name: "위중증자",
+    },
+    {
+      name: "사망자",
+    },
+    {
+      name: "검사자",
+    },
+  ];
+  return (
+    <div className="flex justify-center items-center">
+      <div className="rounded-md shadow-2xl w-4/5 mt-10 border-1 w-full ">
+        <div className="flex justify-start items-center pt-2 pl-4 pb-2">
+          {dataList.map((index, key) => (
+            <div className="cursor-pointer w-20 underline hover:underline-offset-7">
+              {index.name}
+            </div>
+          ))}
         </div>
-    );
+
+        <canvas className="mt-10 border-1 w-full"></canvas>
+      </div>
+    </div>
+  );
 }
 
 export default KoreaChart;
